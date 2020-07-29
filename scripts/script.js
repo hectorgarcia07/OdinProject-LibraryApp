2acef4db3cc80dcbd64c4bcf34b2901fc676e76a
Hector Garcia <hector.garcia07@utrgv.edu>
2020-07-29 18:20:26 -0500

    Set up basic html template and some javascript and css

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+var addBook = document.getElementById("add-book-btn");
+var closeForm = document.getElementById("close-form");
+
+addBook.addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "flex";
+});
+
+closeForm.addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
+
+let myLibrary = [];
+
+function Book() {
+    // the constructor...
+}
+
+function addBookToLibrary() {
+    // do stuff here
+}
+
