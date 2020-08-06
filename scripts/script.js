8ceb6086abff60efb6e2d7b38bb1933062607017
Hector Garcia <hector.garcia07@utrgv.edu>
2020-08-06 11:15:10 -0500

    Added js function to create a new book entry and some style

--- scripts/script.js
+++ scripts/script.js
@@ -10,9 +10,10 @@ let bookContainer = document.getElementById("bookContainer");
 submitBtn.addEventListener("click", function(){
     event.preventDefault();
 
+    //only add and display book if form has information
     if(getTitle.value != "" && getAuthor.value != ""){
-        myLibrary.push(addBookToLibrary(myLibrary.length));
-        document.querySelector(".popup").style.display = "none";
+        myLibrary.push(addBookToLibrary(myLibrary.length));//create column cointaining book info
+        document.querySelector(".popup").style.display = "none";//hide popup
         render(); //display all books
     }
 })
@@ -55,33 +56,46 @@ function render(){
             row.className = "book-row";
             bookContainer.appendChild(row);
         }
+        //add book column to row
         row.appendChild(createBookCol(i));
     }
 }
 
-function createCloseBtn(){
+function createCloseBtn(index){
     let bookBtnContainer = document.createElement('div');
     bookBtnContainer.className = "book-form-close-btn";
 
     let closeBtn = document.createElement('i');
-    //deletes item if clicked
-    closeBtn.addEventListener("click",function(){
-        myLibrary.splice(this.index, 1);
-    })
+
+    /*
+        trying to get this eventListener to work
+    */
+    closeBtn.addEventListener("click", function () {
+        console.log("Testing");
+    });
+    /*
+        Code above not working
+    */
     closeBtn.classList.add("fa", "fa-close", "close-form-btn");
     closeBtn.style.fontSize = "32px";
     bookBtnContainer.appendChild(closeBtn);
+    
     return bookBtnContainer;
 }
 
+//will create a column cointaining book information
 function createBookCol(index){
     let bookItem = document.createElement('div');
-
-    //adds book contents to column
+    
+    //adds style to the column
     bookItem.className = "book-item";
-    //adds close button 
-    bookItem.appendChild(createCloseBtn());
+
+    //adds close button. JAVASCRIPT ISN"T WORKING
+    bookItem.appendChild(createCloseBtn(index));
+
+    //adds information to the column
     bookItem.innerHTML += '<h2>' + myLibrary[index].title + '</h2>'
     bookItem.innerHTML += '<p> Author: ' + myLibrary[index].title + '</p>'
+
     return bookItem;
 }
